1b47a294796c23050710147ea0298541b286c243
Rhys Kyte <rhys.kyte@games-science.com>
2019-09-16 16:09:34 +0100

    zappa tweaks

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, request, redirect, make_response
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 import logging
 import os
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 games = {}
 
-socketURL = os.getenv('SOCKETURL')
-dev_mode = os.getenv('DEVMODE') == 'True'
-host = '0.0.0.0'
-if dev_mode is True:
-    host = socketURL[7:-5]   
+local = os.getenv('LOCAL') == 'True'
+
+if local is True:
+    socketURL = os.getenv('SOCKETURL')
+    host = socketURL[7:-5]
+else:
+    socketURL = 'insert-socket-url-here'
+    host = '0.0.0.0'
 
 def render_view(viewname, params):
     return render_template(viewname, **params)
@@ -21,11 +26,11 @@ def render_view(viewname, params):
 def index():
     return render_view('index.html', {})
 
-@app.route('/games/create', methods=['GET'])
+@app.route('/v1/games/create', methods=['GET'])
 def game_form():
     return render_view('create_game.html', {})
 
-@app.route('/games/create', methods=['POST'])
+@app.route('/v1/games/create', methods=['POST'])
 def create_game():
     name = request.form['game_name']
     while name in games.keys():
@@ -38,14 +43,14 @@ def create_game():
         'slider1': 25,
         'slider2': 0
     }
-    resp = make_response(redirect('/games/all'))
+    resp = make_response(redirect('v1/games/all'))
     return resp
 
-@app.route('/games/all')
+@app.route('/v1/games/all')
 def list_game():
     return render_view('game_list.html', { 'games': games })
 
-@app.route('/games/<game_name>')
+@app.route('/v1/games/<game_name>')
 def join_game(game_name):
      return render_view('game.html', { 'slider1': games[game_name]['slider1'], 'slider2': games[game_name]['slider2'], 'game': game_name, 'socketURL': socketURL })
 
